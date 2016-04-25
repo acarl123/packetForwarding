3f63a40511cadf95bc48f184b2d237edfeaba7af
acarl123 <chuckles.theman@gmail.com>
2016-04-25 15:46:57 -0400

    working on server

--- server/sniffThread.py
+++ server/sniffThread.py
@@ -2,18 +2,20 @@ from scapy.all import *
 import threading, thread
 from scapy.layers.inet import IP
 
+import sys
+sys.path.append('/home/atumsoft/PycharmProjects/packetForwarding/')
 from utils import *
 
 
-IP_ADDRESS = '192.168.0.111'
+IP_ADDRESS = '192.168.0.105'
 
 
 class SniffThread(threading.Thread):
     def __init__(self):
-        pass
+        super(SniffThread, self).__init__()
 
     def run(self):
         while 1:
             p = sniff(iface='atum0', count=1)
 
-            thread.start_new_thread(POST, (p))
+            thread.start_new_thread(POST, (p, IP_ADDRESS))
--- server/writeThread.py
+++ server/writeThread.py
@@ -1,3 +1,5 @@
+import ast
+
 from scapy.all import *
 import threading
 
@@ -5,11 +7,14 @@ import threading
 
 class WriteThread(threading.Thread):
     def __init__(self, q):
+        super(WriteThread, self).__init__()
         self.q = q
 
     def run(self):
         while 1:
             if not self.q: continue
-
-            p = Raw(self.q.get())
+            p = ast.literal_eval(ast.literal_eval(self.q.get()))
+            p = ''.join([chr(i) for i in p])
+            print p
+            p = Raw(p)
             sendp(p, iface='atum0')
