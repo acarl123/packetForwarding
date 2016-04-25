c29d266167210d896db8d6c06a5889beb50b488f
acarl123 <chuckles.theman@gmail.com>
2016-04-25 15:46:38 -0400

    working on client code

--- client/sniffThread.py
+++ client/sniffThread.py
@@ -1,7 +1,11 @@
 from scapy.all import *
 import threading, thread
 
+import sys
 
+from scapy.layers.inet import IP
+
+sys.path.append('/home/atumsoft/PycharmProjects/packetForwarding/')
 from utils import *
 
 
@@ -10,11 +14,13 @@ IP_ADDRESS = '192.168.0.111'
 
 class SniffThread(threading.Thread):
     def __init__(self):
-        pass
+        super(SniffThread, self).__init__()
 
     def run(self):
         while 1:
             p = Ether()/IP()/'Hello World'
             time.sleep(2)
 
+            p = [ord(c) for c in str(p)]
+            print p
             thread.start_new_thread(POST, (p, IP_ADDRESS))
